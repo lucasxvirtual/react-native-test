d4e50d1c35c8a7158f1229d103ec750a3525bff5
Lucas Silva <lucas18_2006@hotmail.com>
2020-01-02 17:58:36 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const googleWebClientId = '1043910174396-7ttghfpa74osp3106pafoj7ahp4e72ue.apps.googleusercontent.com'
+export const googleWebClientId = '1043910174396-7ttghfpa74osp3106pafoj7ahp4e72ue.apps.googleusercontent.com' // for test only, replace with your own
 export const baseUrl = 'http://167.99.51.114/'
